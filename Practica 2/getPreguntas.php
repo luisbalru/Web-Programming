1e202aff765328d0dabbb75f41dcae16d4d7f49f
luisbalru <balderasruizluis@gmail.com>
2018-05-31 21:05:45 +0200

    Obtención de preguntas y respuestas del foro

--- Practica 2/getPreguntas.php
+++ Practica 2/getPreguntas.php
@@ -1,41 +1,62 @@
 <?php
-  require_once('GestionPreguntas.class.inc');
+require_once('GestionPreguntas.class.inc');
+require_once('GestionCliente.class.inc');
 
-  $gestionpregunta = new GestionPreguntas();
-  $gestioncliente = new GestionCliente();
-  $preguntas = $gestionpregunta->obtenerTodasPreguntas();
+  function getPreguntas(){
+    $gestionpregunta = new GestionPreguntas();
+    $gestioncliente = new GestionCliente();
+    $preguntas = $gestionpregunta->obtenerTodasPreguntas();
 
-  foreach ($preguntas as $pregunta) {
-    $dni = $pregunta['usuario'];
-    $inform_usuario = $gestioncliente->obtenerCliente($dni);
-    $nombre = $inform_usuario['Nombre'];
-    $foto = $inform_usuario['NombreImagen'];
-    $query = $pregunta['id'];
-    $respuestas = $gestionpregunta->obtenerRespuestas($query);
+    foreach ($preguntas as $pregunta) {
+      $dni = $pregunta['usuario'];
+      $inform_usuario = $gestioncliente->obtenerCliente($dni);
+      $nombre = $inform_usuario['Nombre'];
+      $foto = $inform_usuario['NombreImagen'];
+      $query = $pregunta['id'];
+      $respuestas = array();
+      $respuestas = $gestionpregunta->obtenerRespuestas($query);
 
-    echo '<section class="respuesta">
-            <form action="add_respuesta.php?id="'.$query.'" method="post">
-              <h2>PREGUNTA</h2>
-                <a href="#" data-toggle="tooltip" data-placement="top" title="'.getQueries($dni).'"><img src='.$foto.' width="50px" height="50px"></a>
-                <h4>'.$nombre.': '.$pregunta['query'].'</h4>';
-    foreach($respuestas as $respuesta){
-      $dni_respuesta = $respuesta['usuario'];
-      $datos_answer = $gestioncliente->obtenerCliente($dni_respuesta);
-      foreach($datos_answer as $responder)
-      {
-        echo '<img src='.$responder['NombreImagen'].' width="30px" height="30px"</a>
-              <p>'.$responder['Nombre'].' : '.$respuesta['topic'].'</p>';
+      echo '<section class=container><br>';
+      echo '<section class="respuesta">
+              <form action="add_respuesta.php?id='.$query.'" method="post">
+                <h2>PREGUNTA</h2>
+                  <span title="Un momento.." id="'.$dni.'"><img src="'.$foto.'" width="50px" height="50px"></span>
+                  <h4>'.$nombre.': '.$pregunta['topic'].'</h4>';
+
+      if(!empty($respuestas)){
+        echo '<h3>RESPUESTAS</h3>';
+        foreach($respuestas as $respuesta){
+          $dni_respuesta = $respuesta['usuario'];
+          $responder = $gestioncliente->obtenerCliente($dni_respuesta);
+
+
+          echo  '<img src='.$responder['NombreImagen'].' width="30px" height="30px"</a>
+                  <p>'.$responder['Nombre'].' : '.$respuesta['topic'].'</p>';
+
+
+
+          echo '
+          <br/>
+          <br/>
+          <br/>
+          <br/>';
+        }
+        echo'<textarea name="mensaje" placeholder="Escriba una nueva respuesta"></textarea>
+            <input type="submit" value="ENVIAR">
+            </form>
+            </section>';
       }
-
-      echo'<textarea name="mensaje" placeholder="Escriba una nueva respuesta"></textarea>
-          <input type="submit" value="ENVIAR" id="bot">
-          </form>
-          </section>';
-      echo '
-      <br/>
-      <br/>
-      <br/>
-      <br/>';
+      else{
+        echo'<textarea name="mensaje" placeholder="Escriba una nueva respuesta"></textarea>
+            <input type="submit" value="ENVIAR">
+            </form>
+            </section>';
+        echo '
+        <br/>
+        <br/>
+        <br/>
+        <br/>';
+      }
     }
   }
 
